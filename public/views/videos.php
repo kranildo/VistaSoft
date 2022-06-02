b457d0a4050b99a2a89bb5ecb85e672bac537402
MaktubLTDA <95450345+MaktubLTDA@users.noreply.github.com>
2022-06-02 13:55:18 -0300

    Componente Videos

---
+++ public/views/videos.php
@@ -0,0 +1,5 @@
+<h3>Videos</h3>
+<div class="col-md-12"><iframe width="560" height="315" src="https://www.youtube.com/embed/RfVUiAVKj0A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+<div class="col-md-12"><iframe width="560" height="315" src="https://www.youtube.com/embed/0noP67FBpUQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
